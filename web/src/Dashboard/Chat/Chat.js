d73059d8718298c28723d7a10e08ce02ac3add00
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-09-01 20:26:57 +0300

    feat: add ChatLogo component and integrate it into Chat component

--- web/src/Dashboard/Chat/Chat.js
+++ web/src/Dashboard/Chat/Chat.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+const ChatLogo = () => {
+  return (
+    <div className="chat_gpt_logo_container">
+      <p className="chat_gpt_logo">ChatGPT</p>
+    </div>
+  );
+};
 const Chat = () => {
-  return <div className="chat_container">Chat</div>;
+  return (
+    <div className="chat_container">
+      <ChatLogo />
+    </div>
+  );
 };
 
 export default Chat;
