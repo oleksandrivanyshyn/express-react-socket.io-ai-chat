16381b22eb3899dc3fcbd2b0de38e37a4fa00683
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-09-01 20:35:28 +0300

    feat: add Message component and integrate it into Messages component

---
+++ web/src/Dashboard/Chat/Message.js
@@ -0,0 +1,30 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { GrUser } from 'react-icons/gr';
+import { FcMindMap } from 'react-icons/fc';
+
+const SlowText = (props) => {
+  const { speed, text } = props;
+
+  const [placeholder, setPlaceholder] = useState(text[0]);
+
+  const index = useRef(0);
+
+  useEffect(() => {
+    function tick() {
+      index.current++;
+      setPlaceholder((prev) => prev + text[index.current]);
+    }
+    if (index.current < text.length - 1) {
+      let addChar = setInterval(tick, speed);
+      return () => clearInterval(addChar);
+    }
+  }, [placeholder, speed, text]);
+
+  return <span>{placeholder}</span>;
+};
+
+const Message = ({ content, aiMessage, animate }) => {
+  return <div>Message</div>;
+};
+
+export default Message;
