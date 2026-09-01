52acc72c6471964c552b7b85c930ca2186f33e0b
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-09-01 20:23:20 +0300

    feat: add ListItem component and integrate it into Sidebar

--- web/src/Dashboard/Sidebar/Sidebar.js
+++ web/src/Dashboard/Sidebar/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import NewChatButton from './NewChatButton';
+import ListItem from './ListItem';
 
 const Sidebar = () => {
   return (
     <div className="sidebar_container">
       <NewChatButton />
+      <ListItem title="history 1" />
+      <ListItem title="history 2" />
+      <ListItem title="history 3" />
     </div>
   );
 };
